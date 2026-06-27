9893b49085c60913898ec8a60f9700569bdf39a3
quiser <quiser714@outlook.com>
2026-06-27 08:50:59 +0800

    通过code.InteractiveConsole模拟一个REPL

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-import os
+import sys
+from typing import Any
+from code import InteractiveConsole
+
+if sys.stdin.isatty() and hasattr(sys, "__interactivehook__"):
+    sys.__interactivehook__()  # noqa
 
 
-class Clear:
-    def __repr__(self) -> str:
-        clear_cmd = "cls" if os.name == "nt" else "clear"
-        os.system(clear_cmd)
-        return ""
+class PythonPlus(InteractiveConsole):
+    def __init__(
+        self, locals: dict[str, Any] | None = None, filename: str = "<console>"
+    ) -> None:
+        super().__init__(locals, filename)
+
+        self._banner: str = (
+            f"Python {sys.version} on {sys.platform}\n"
+            'Type "help", "copyright", "credits" or "license" for more information.'
+        )
+
+    def interact(self, banner: str | None = None, exitmsg: str | None = None) -> None:
+        _banner = banner if banner else self._banner
+        return super().interact(_banner, exitmsg)
 
 
-class ListDir:
-    def __repr__(self) -> str:
-        list_dir_cmd = "dir" if os.name == "nt" else "ls"
-        os.system(list_dir_cmd)
-        return ""
+python_plus = PythonPlus()
 
-
-clear = Clear()
-ls = ListDir()
+python_plus.interact()
